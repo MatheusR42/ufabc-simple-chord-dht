178dee480e338677f239ae84a96c15a778c2888a
matheuaraujo_meli <matheus.araujo@mercadolivre.com>
2024-09-07 00:11:13 -0300

    add more config

--- node/src/node.py
+++ node/src/node.py
@@ -6,24 +6,47 @@ from signal import signal, SIGTERM, SIGINT, pause
 import threading
 import os
 
-def serve():
+# Interactive terminal function
+def interactive_terminal():
+    while True:
+        command = input("Enter command (type 'exit' to quit): ")
+        if command == 'exit':
+            print("Exiting interactive terminal...")
+            break
+        else:
+            print(f"Received command: {command}")
+
+# gRPC server function
+def serve(server):
+    server.start()
+    print("Running gRPC server")
+    server.wait_for_termination()
+
+# Thread to run both server and terminal
+if __name__ == "__main__":
+    # Create the gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-
     servicer = MeuQoelhoMqServicer()
-
     meu_qoelho_mq_pb2_grpc.add_MeuQoelhoMqServicer_to_server(servicer, server)
     server.add_insecure_port("[::]:50051")
 
-    server.start()
-    print("running server")
+    # Create thread for the gRPC server
+    server_thread = threading.Thread(target=serve, args=(server,))
+    server_thread.start()
 
-    def handle_termination(*_: any) -> None :
-      server.stop(None)
-      servicer.service.clear_subs()
-      print('Cleared subs')
-      os._exit(0)
+    # Handle termination signals in the main thread
+    def handle_termination(*_: any) -> None:
+        print("Shutting down server...")
+        server.stop(0)
+        servicer.service.clear_subs()
+        print('Cleared subs')
+        os._exit(0)
 
     signal(SIGINT, handle_termination)
     signal(SIGTERM, handle_termination)
-    server.wait_for_termination()
-serve()
+
+    # Run the interactive terminal in the main thread
+    interactive_terminal()
+
+    # Wait for the server thread to finish (this will happen when the server is terminated)
+    server_thread.join()
--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Navigate to the directory containing the requirements.txt file
+cd ./node
+
+# Install the requirements
+pip install -r requirements.txt
 
 # Navigate to the directory containing the generate-python-protos.sh script
-cd ./protos
+cd ../protos
 
 # Make sure the script is executable
 chmod +x generate-python-protos.sh
@@ -9,10 +14,7 @@ chmod +x generate-python-protos.sh
 # Run the generate-python-protos.sh script
 ./generate-python-protos.sh
 
-# Navigate to the directory containing the requirements.txt file
-cd ../node
-
-# Install the requirements
-pip install -r requirements.txt
+# Copy "protocols" folder to client-python folder
+cp -r ../node/src/protocols ../client-python/src/
 
 echo "All tasks completed successfully."
